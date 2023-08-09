38ec20fd38fdbabe3b6659c573b1e85f91176c06
Julius Kiekbusch <contact@julius-kiekbusch.de>
2023-08-09 15:02:50 +0200

    Remove deprecated function_typehint_space rule

--- src/Config.php
+++ src/Config.php
@@ -69,7 +69,6 @@ class Config extends \PhpCsFixer\Config
         'full_opening_tag' => true,
         'fully_qualified_strict_types' => true,
         'function_declaration' => true,
-        'function_typehint_space' => true,
         'general_phpdoc_tag_rename' => true,
         'heredoc_to_nowdoc' => true,
         'include' => true,
