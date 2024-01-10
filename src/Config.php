cbd6afd0888254e75a23c040625bae7a1834567f
Julius Kiekbusch <contact@julius-kiekbusch.de>
2024-01-10 10:44:22 +0100

    Disables fully_qualified_strict_types

--- src/Config.php
+++ src/Config.php
@@ -68,7 +68,7 @@ class Config extends \PhpCsFixer\Config
         'elseif' => true,
         'encoding' => true,
         'full_opening_tag' => true,
-        'fully_qualified_strict_types' => true,
+        'fully_qualified_strict_types' => false,
         'function_declaration' => true,
         'general_phpdoc_tag_rename' => true,
         'heredoc_to_nowdoc' => true,
